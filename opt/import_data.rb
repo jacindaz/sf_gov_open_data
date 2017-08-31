c9c684ea3f194266ca42f28a334bea30aee8071f
Jacinda <jacindaz@gmail.com>
2017-08-31 08:39:36 -0700

    Add create_table method for setting up import of data

--- opt/import_data.rb
+++ opt/import_data.rb
@@ -6,21 +6,33 @@ require 'pry'
 class ImportSFPoliceData
   def initialize(url='https://data.sfgov.org/resource/cuks-n6tp.json')
     @url = url
-    @connection = PG.connect( dbname: 'sf_police_data_development' )
+    @connection = PG.connect(dbname: 'sf_police_data_development')
     @table_name = 'original_sf_gov_data'
+  end
+
+  def json_from_file
+    puts "reading the json file..."
+    file = File.read("/Users/jzhong/Documents/jacinda/sf_police_data/opt/original_sf_gov_data.json")
+
+    @results = JSON.parse(file)
   end
 
   def json_response
     response = Net::HTTP.get(URI(@url))
     @json_response ||= JSON.parse(response)
+
+  private
+
+  def table_columns
+    @table_columns ||= @results.first.keys.map!{ |c| c.sub(/:@/,'') }
   end
 
   def create_table
-    columns = json_response.first.keys.map!{ |c| c.sub(/:@/,'') }
+    puts "creating table...."
 
-    create_table_sql = "create table #{@table_name} ("
-    create_table_sql += "id integer PRIMARY KEY,"
-    create_table_sql += (columns.join(" varchar(255), ") + " varchar(255)")
+    create_table_sql = "create table if not exists #{@table_name} ("
+    create_table_sql += "id serial primary key,"
+    create_table_sql += (table_columns.join(" varchar(255), ") + " varchar(255)")
     create_table_sql += ");"
 
     @connection.exec(create_table_sql)
