4efc8ebd5b397dc4c956fd2532e1d4ca48879321
Jacinda <jacindaz@gmail.com>
2017-08-30 20:14:50 -0700

    connect to database and create table based on JSON response

--- opt/import_data.rb
+++ opt/import_data.rb
@@ -1,12 +1,33 @@
 require 'net/http'
 require 'json'
+require 'pg'
+require 'pry'
 
 class ImportSFPoliceData
-  attr_reader :json_response
+  def initialize(url='https://data.sfgov.org/resource/cuks-n6tp.json')
+    @url = url
+    @connection = PG.connect( dbname: 'sf_police_data_development' )
+    @table_name = 'original_sf_gov_data'
+  end
 
-  def initialize
-    uri = URI('https://data.sfgov.org/resource/cuks-n6tp.json')
-    response = Net::HTTP.get(uri)
-    @json_response = JSON.parse(response)
+  def json_response
+    response = Net::HTTP.get(URI(@url))
+    @json_response ||= JSON.parse(response)
+  end
+
+  def create_table
+    columns = json_response.first.keys.map!{ |c| c.sub(/:@/,'') }
+
+    create_table_sql = "create table #{@table_name} ("
+    create_table_sql += "id integer PRIMARY KEY,"
+    create_table_sql += (columns.join(" varchar(255), ") + " varchar(255)")
+    create_table_sql += ");"
+
+    @connection.exec(create_table_sql)
+  end
+
+  def rollback
+    @connection.exec("drop table #{@table_name}")
   end
 end
+
