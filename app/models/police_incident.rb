c13d4f3930786e50d64f5107d0cce33a873b00b5
Jacinda <jacindaz@gmail.com>
2017-09-04 15:02:45 -0700

    CSV import that works! Plus PoliceIncident model with app-level validations

---
+++ app/models/police_incident.rb
@@ -0,0 +1,18 @@
+class PoliceIncident < ActiveRecord::Base
+  validates :incident_number, numericality: true
+  validates :category, presence: true
+  validates :description, presence: true
+
+  validates :day_of_week, inclusion: { in: %w(Monday Tuesday Wednesday Thursday Friday Saturday Sunday), message: "%{value} must be a day of the week" }
+  validates :date, presence: true
+  validates :time, presence: true
+  validates :address, presence: true
+
+  validates :police_department_district_id, numericality: true
+  validates :police_department_district_name, presence: true
+  validates :resolution, presence: true
+
+  validates :x_coordinate, presence: true
+  validates :y_coordinate, presence: true
+  validates :location, presence: true
+end
--- lib/csv_importer.rb
+++ lib/csv_importer.rb
@@ -1,4 +1,5 @@
 require 'csv'
+require_relative '../app/models/police_incident'
 
 class CsvImporter
   def initialize(file_path)
@@ -6,7 +7,28 @@ class CsvImporter
   end
 
   def process
-    result = CSV.read(@file_path, headers: :downcase, converters: :all, header_converters: :downcase)
-    result.map(&:to_h)
+    PoliceIncident.transaction do
+      result = CSV.read(@file_path, headers: :downcase, converters: :all, header_converters: :downcase)
+
+      result.each do |row|
+        new_pi = PoliceIncident.new(
+          incident_number: row["incidntnum"].to_i,
+          category: row["category"].downcase,
+          description: row["descript"].capitalize,
+          day_of_week: row["dayofweek"].capitalize,
+          date: Date.strptime(row["date"], '%m/%d/%y'),
+          time: row["time"].to_time,
+          police_department_district_name: row["pddistrict"].capitalize,
+          resolution: row["resolution"].downcase,
+          address: row["address"].capitalize,
+          x_coordinate: row["x"].to_i,
+          y_coordinate: row["y"].to_i,
+          location: row["location"],
+          police_department_district_id: row["pdid"].to_i,
+        )
+
+        new_pi.save!
+      end
+    end
   end
 end
