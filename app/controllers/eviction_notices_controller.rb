393a34d6752d0ae6645994daff967f16725d5e08
Jacinda <jacindaz@gmail.com>
2017-12-27 14:09:22 -0800

    Move query editor to partial, separate saving a query and rendering results of a query

--- app/controllers/eviction_notices_controller.rb
+++ app/controllers/eviction_notices_controller.rb
@@ -1,31 +1,38 @@
 class EvictionNoticesController < ApplicationController
   def index
-    if params[:query_id]
-      query = Query.find(params[:query_id])
-      query_results = query.results
-      @evictions = EvictionNotice.where(eviction_id: query_results)
-    else
-      @evictions = EvictionNotice.limit(10)
-    end
+    @evictions = EvictionNotice.limit(10)
+  end
+
+  def show
+    query = Query.find(params[:id])
+    query_results = query.results
+    @evictions = EvictionNotice.where(eviction_id: query_results)
+
+    render :index
   end
 
   def run_query
     # fix this!!! sql injection!!! need to sanitize!!!
     results = ActiveRecord::Base.connection.exec_query(query_params)
     row_ids = results.rows.map(&:first)
-    evictions = EvictionNotice.where(eviction_id: row_ids)
+    @evictions = EvictionNotice.where(eviction_id: row_ids)
 
-    new_query = Query.new(query: query_params, results: row_ids)
-    if new_query.save!
-      redirect_to eviction_notices_path(query_id: new_query.id)
-    else
-      render :index, notice: 'Query could not be executed!'
-    end
+    render :index, collection: @evictions
   end
 
   private
 
   def query_params
     params[:query]
+  end
+
+  def save_query
+    new_query = Query.new(query: query_params, results: row_ids)
+    if new_query.save!
+      render :index, collection: @evictions
+      redirect_to eviction_notice_path(query_id: new_query.id)
+    else
+      render :index, notice: "Query could not be executed!"
+    end
   end
 end
