310ff5a3a37e3e007cc8036ac3dbf118657f82c0
Jacinda <jacindaz@gmail.com>
2018-01-02 11:53:30 -0800

    EvictionNotice controller request specs

--- spec/requests/eviction_notices_spec.rb
+++ spec/requests/eviction_notices_spec.rb
@@ -7,4 +7,19 @@ RSpec.describe "EvictionNotices", type: :request do
       expect(response).to have_http_status(200)
     end
   end
+
+  describe "GET#show" do
+    it "renders successfully!" do
+      get eviction_notice_path(create :valid_query)
+      expect(response).to have_http_status(200)
+    end
+  end
+
+  describe "GET#run_query" do
+    it "successfully runs a query with valid sql" do
+    end
+
+    it "returns an error when sql is invalid" do
+    end
+  end
 end
