8d37e9175ba470a2294b7a52199a09b990bef9e0
Jacinda <jacindaz@gmail.com>
2018-01-10 19:58:33 -0800

    Log errors if the table was unable to be copied/created

--- opt/setup_table.rb
+++ opt/setup_table.rb
@@ -16,14 +16,21 @@ class SetupTable
     sql += "CREATE TABLE #{@new_table_name} AS SELECT * FROM #{@copied_table_name};"
     @connection.exec(sql)
 
-    verify_table_exists = @connection.exec("select table_catalog, table_schema, table_name from information_schema.tables where table_name = '#{@new_table_name}';").first
+    table_exists = @connection.exec("select table_catalog, table_schema, table_name from information_schema.tables where table_name = '#{@new_table_name}';").first
 
-    @logger.info("New table created!")
-    @logger.info("Table: #{verify_table_exists['table_name']}, Schema: #{verify_table_exists['table_schema']}, Database: #{verify_table_exists['table_catalog']}")
+    if table_exists
+      @logger.info("New table created!")
+      @logger.info("Table: #{table_exists['table_name']}, Schema: #{table_exists['table_schema']}, Database: #{table_exists['table_catalog']}")
+    else
+      @logger.error("Table #{@new_table_name} was unable to be created. Boooo.")
+    end
+
   end
 
   def add_primary_key
     add_primary_key = "ALTER TABLE #{@new_table_name} ADD COLUMN id SERIAL PRIMARY KEY;"
     @connection.exec(add_primary_key)
+
+    @logger.info("Added a primary key to the #{@new_table_name} table!")
   end
 end
