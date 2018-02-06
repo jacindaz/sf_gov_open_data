57fc82eda40d32b790bfad4cdc0fbfe74e48af13
Jacinda Zhong <jacindaz@gmail.com>
2018-02-05 18:12:01 -0800

    Make JSON importing work for multiple endpoints/tables
     - pull from the data_sources table to find which 
       endpoint to use, and which table to import into
     - also eventually will use unique identifier column
       to figure out which rows have already been imported

--- opt/import_data.rb
+++ opt/import_data.rb
@@ -4,22 +4,30 @@ require 'pg'
 require 'pry'
 
 class ImportData
-  def initialize(database_name, drop_table=false)
+  def initialize(database_name, drop_tables=false)
     @connection = PG.connect(dbname: database_name)
-    @drop_table = drop_table
+    @drop_table = drop_tables
   end
 
-  def json_response
-    response = Net::HTTP.get(URI(@url))
+  def json_response(url)
+    response = Net::HTTP.get(URI(url))
     puts "parsing json response..."
-    @results ||= JSON.parse(response)
+    JSON.parse(response)
   end
 
   def process
-    rollback if @drop_table
-    json_response
-    create_table
+  	DataSource.all.each do |data_source|
+  	  rollback(data_source.table_name) if @drop_tables
 
+  	  results = json_response(data_source.json_endpoint)
+      table_columns = results.first.keys.map!{ |c| c.sub(/:@/,'') }
+
+  	  create_table(data_source.table_name, table_columns)
+  	  insert_rows(data_source, table_columns, results)
+    end
+  end
+
+  def insert_rows(data_source, table_columns, results)
     puts "inserting values into table..."
     @connection.prepare('insert_statement', "insert into #{@table_name} (#{table_columns.join(', ')}) values ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17, $18)")
     @results.each do |row|
@@ -30,14 +38,10 @@ class ImportData
 
   private
 
-  def table_columns
-    @table_columns ||= @results.first.keys.map!{ |c| c.sub(/:@/,'') }
-  end
-
-  def create_table
+  def create_table(table_name, table_columns)
     puts "creating table...."
 
-    create_table_sql = "create table if not exists #{@table_name} ("
+    create_table_sql = "create table if not exists #{table_name} ("
     create_table_sql += "id serial primary key,"
     create_table_sql += (table_columns.join(" varchar(255), ") + " varchar(255)")
     create_table_sql += ");"
@@ -45,10 +49,8 @@ class ImportData
     @connection.exec(create_table_sql)
   end
 
-  def rollback
-    @connection.exec("drop table #{@table_name}") 
+  def rollback(table_name)
+    @connection.exec("drop table #{table_name}")
   end
 end
 
-i = ImportSFPoliceData.new
-i.process
