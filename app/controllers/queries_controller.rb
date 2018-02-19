10aff61c4bcb40dd1f7a1f04a93ce588e82dd959
Jacinda Zhong <jacindaz@gmail.com>
2018-02-19 12:29:22 -0800

    Display query results and dynamically display column and row values

--- app/controllers/queries_controller.rb
+++ app/controllers/queries_controller.rb
@@ -11,6 +11,9 @@ class QueriesController < ApplicationController
     @new_query = Query.new
   end
 
+  def show
+  end
+
   def create
     results = ActiveRecord::Base.connection.exec_query(query_params)
     row_ids = results.rows.map(&:first)
@@ -23,9 +26,38 @@ class QueriesController < ApplicationController
     end
   end
 
+  def run_query
+    @running_query = Query.new(query: query_params, results: [])
+
+    begin
+      results = ActiveRecord::Base.connection.exec_query(query_params)
+    rescue ActiveRecord::StatementInvalid
+      results = nil
+      flash[:notice] = "Query syntax is invalid."
+    end
+
+    if !valid_query
+      flash[:notice] = "#{Query::INVALID_SQL.join(', ')} statements cannot be run."
+      @queries = []
+      render :index
+    elsif results.nil?
+      @queries = []
+      render :index
+    else
+      @results = results
+      render :show
+    end
+  end
+
   private
 
   def query_params
     params[:query]
+  end
+
+  def valid_query
+    # hmm, should i sanitize by not allowing drop/delete
+    # or sanitize by only allowing selects/with/etc ??
+    Query::INVALID_SQL.none?{ |invalid| query_params.include?(invalid) }
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "queries#index"
-  resources :eviction_notices, only: [:index, :show] do
+  resources :eviction_notices, only: [:index, :show]
+
+  resources :queries, only: [:index, :show, :create] do
     collection { post :run_query }
   end
-
-  resources :queries, only: [:index, :create]
 end
