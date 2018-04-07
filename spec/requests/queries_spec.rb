05591b2637a75ee4b9cc8a34328c379b5db5627c
Jacinda Zhong <jacindaz@gmail.com>
2018-04-07 10:14:59 -0700

    Tests! for run valid/invalid queries, and for saving new queries

--- spec/requests/queries_spec.rb
+++ spec/requests/queries_spec.rb
@@ -11,16 +11,26 @@ RSpec.describe "Queries", type: :request do
   describe "GET#run_query" do
     it "successfully runs a query with valid sql" do
       valid_query = create :valid_query
-      post run_query_path(query: valid_query.query)
+
+      post run_query_path(query: valid_query.attributes)
       expect(response).to have_http_status(200)
     end
 
     it "returns an error when sql is invalid" do
       invalid_query = create :invalid_query
-      post run_query_path(query: invalid_query.query)
+      post run_query_path(query: invalid_query.attributes)
 
       expect(response).to have_http_status(200)
       expect(response.body).to include("cannot be run")
+    end
+  end
+
+  describe "POST#create" do
+    it "should save queries successfully" do
+      valid_query = build :valid_query
+      post queries_path(query: valid_query.attributes)
+
+      expect(response).to redirect_to(queries_path)
     end
   end
 end
